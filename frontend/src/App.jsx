b6d2f601f4a1a17336302741d954daf3a8924b6a
akib-siddique22 <akibs2001@gmail.com>
2024-08-24 01:41:56 +0800

    edits to reactor router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateMovie from './pages/CreateMovies';
+import ShowMovie from './pages/ShowMovie';
+import EditMovie from './pages/EditMovie';
+import DeleteMovie from './pages/DeleteMovie';
 
 const App = () => {
   return (
-    <div className = 'bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/movies/create' element={<CreateMovie/>} />
+      <Route path='/movies/details/:id' element={<ShowMovie/>} />
+      <Route path='/movies/edit/:id' element={<EditMovie/>} />
+      <Route path='/movies/delete/:id' element={<DeleteMovie/>} />
+    </Routes>
   )
 }
 
