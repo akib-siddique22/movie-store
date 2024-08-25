b1d1bad26f1e8b1f821788be54c5ed94fa246bb1
akib-siddique22 <akibs2001@gmail.com>
2024-08-26 00:04:27 +0800

    changed cors

--- backend/index.js
+++ backend/index.js
@@ -12,15 +12,15 @@ app.use(express.json());
 
 //Middleware for CORS Policy
 //Option 1: Allow All Origins
-//app.use(cors());
+app.use(cors());
 //Options 2: Allow Custom Origins
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', "DELETE"],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', "DELETE"],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (request, response) => {
     console.log(request);
