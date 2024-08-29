e3f6d3ff42ab5e7aa3414bf29d99fa7fd626e607
akib-siddique22 <akibs2001@gmail.com>
2024-08-29 19:22:31 +0800

    added movie modal

--- frontend/src/components/home/MovieSingleCard.jsx
+++ frontend/src/components/home/MovieSingleCard.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import {PiBookOpenTextLight} from 'react-icons/pi';
-import {BiUserCircle} from 'react-icons/bi';
+import {BiUserCircle, BiShow} from 'react-icons/bi';
 import {AiOutlineEdit} from 'react-icons/ai';
 import {BsInfoCircle} from 'react-icons/bs';
 import {MdOutlineDelete} from 'react-icons/md';
+import { useState } from "react";
+import MovieModal from "./MovieModal";
 
 const MovieSingleCard = ({movie}) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div
     key={movie._id}
@@ -24,6 +28,10 @@ const MovieSingleCard = ({movie}) => {
       <h2 className="my-1">{movie.director}</h2>
     </div>
     <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+      <BiShow
+        className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+        onClick={() => setShowModal(true)}
+      />
       <Link to={`/movies/details/${movie._id}`}>
         <BsInfoCircle className="text-2xl text-green-800 hover:text-black"/>
       </Link>
@@ -34,6 +42,11 @@ const MovieSingleCard = ({movie}) => {
         <MdOutlineDelete className="text-2xl text-red-600 hover:text-black"/>
       </Link>
     </div>
+    {
+      showModal && (
+        <MovieModal movie={movie} onClose={() => setShowModal(false)} />
+      )
+    }
 </div>
   )
 }
