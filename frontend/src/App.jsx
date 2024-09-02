623c92e26495c0a6640f7933ad4114823418f3dd
akib-siddique22 <akibs2001@gmail.com>
2024-09-03 00:53:39 +0800

    added routes for sign up and login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import CreateMovie from './pages/CreateMovies';
 import ShowMovie from './pages/ShowMovie';
 import EditMovie from './pages/EditMovie';
 import DeleteMovie from './pages/DeleteMovie';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
 
 const App = () => {
   return (
@@ -14,6 +18,8 @@ const App = () => {
       <Route path='/movies/details/:id' element={<ShowMovie />} />
       <Route path='/movies/edit/:id' element={<EditMovie />} />
       <Route path='/movies/delete/:id' element={<DeleteMovie />} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<SignUp/>} />
     </Routes>
   );
 };
