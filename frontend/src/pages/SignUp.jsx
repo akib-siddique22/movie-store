1ee07891df9633af30df1dca43210a80313f23ed
akib-siddique22 <akibs2001@gmail.com>
2024-09-07 00:18:29 +0800

    link to login page, connected sign up page to backend

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,24 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
+import Spinner from '../components/Spinner';
+
 
 const SignUp = () => {
+  const [username, setUser] = useState('')
+  const [password, setPass] = useState('')
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+  const Submission = (e) => {
+    e.preventDefault()
+    const data = {
+      username,
+      password,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:5555/users/signup', data)
+      .then(() => {
+        setLoading(false);
+        enqueueSnackbar('Registered Successfully', {variant: 'success'});
+        navigate('/login');
+      })
+      .catch((error) => {
+        setLoading(false);
+        //alert('An error has occured');
+        enqueueSnackbar('Error', {variant: 'error'});
+        console.log(error);
+      });
+    };
+
+
   return (
-    <div class="flex justify-center items-center h-screen">
-      <div class="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+    <div className="flex justify-center items-center h-screen">
+      {loading ? <Spinner />: ''}
+      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
         <form>
-          <div class="mb-4">
-            <label for="username" class="block text-sm font-medium text-gray-700">USERNAME</label>
-            <input type="text" id="username" name="username" class="form-input mt-1 block w-full h-10 rounded-md shadow-sm border border-black px-3"/>
+          <div className="mb-4">
+            <label form="username" className="block text-sm font-medium text-gray-700">USERNAME</label>
+            <input 
+              type="text" 
+              className="form-input mt-1 block w-full h-10 rounded-md shadow-sm border border-black px-3" 
+              onChange={(e) => setUser(e.target.value)}
+            />
           </div>
 
-          <div class="mb-4">
-            <label for="password" class="block text-sm font-medium text-gray-700">PASSWORD</label>
-            <input type="password" id="password" name="password" class="form-input mt-1 block w-full h-10 rounded-md shadow-sm border border-black px-3"/>
+          <div className="mb-4">
+            <label form="password" className="block text-sm font-medium text-gray-700">PASSWORD</label>
+            <input 
+              type="password" 
+              className="form-input mt-1 block w-full h-10 rounded-md shadow-sm border border-black px-3" 
+              onChange={(e) => setPass(e.target.value)}
+            />
+          </div>
+          
+
+          <div className="mb-6">
+            <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={Submission} >Sign Up</button>
           </div>
 
-          <div class="mb-6">
-            <button type="submit" class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Up</button>
+          </form>
+
+          <div className="mb-2">
+            <p className="text-sm text-gray-600">Already have an account?</p>
           </div>
-        </form>
+
+          <div className="mb-6 w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded text-center">
+          <Link to="/login" className='block w-full'>Login</Link>
+          </div>
+
       </div>
     </div>
   )
