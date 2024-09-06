03aeaaba57221573ee31c4674cd584050722430b
akib-siddique22 <akibs2001@gmail.com>
2024-09-07 00:17:17 +0800

    backend route for users

---
+++ backend/routes/usersRoute.js
@@ -0,0 +1,60 @@
+import express from 'express';
+import { User } from '../models/userModel.js';
+
+const router = express.Router();
+
+//Sign up
+router.post("/signup", async (request, response) => {
+    try {
+        if (
+            !request.body.username ||
+            !request.body.password
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: username, password'
+            });
+        }
+        const newUser = {
+            username: request.body.username,
+            password: request.body.password,
+        };
+
+        const user = await User.create(newUser);
+
+        return response.status(201).send(user);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//Login
+router.post('/login', async (request, response) => {
+    try {
+        if (
+            !request.body.username ||
+            !request.body.password
+        ) {
+            return response.status(400).send({
+                message: 'Need to enter both your username and password'
+            });
+        }
+        const usern = request.body.username
+        const pass = request.body.password
+        const user = await User.findOne({ username: usern })
+        if (user) {
+            if (user.password === pass) {
+                response.json("Successfully Logged In")
+            } else {
+                response.json("Incorrect Details")
+            }
+        } else {
+            response.json("Incorrect Details")
+        }
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+export default router
