970493801c1173f72fea492eb8e4a51f7687f91f
akib-siddique22 <akibs2001@gmail.com>
2024-11-18 05:28:31 +0800

    added cart array when user adds to cart

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,6 +10,10 @@ const userSchema = mongoose.Schema(
             type: String,
             required: true,
         },
+        cart:{
+            type: Array,
+            required: false
+        }
     },
     {
         timestamps: true,
